6e07c6a35b72690e26bfbca3bb992d55c5f588a8
imorosanu <chrnosnow@gmail.com>
2025-08-28 15:48:55 +0300

    refactor (ui): refactor(ui): Refine color scheme for header and themes

---
+++ frontend/src/ThinkingBubble.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ThinkingBubble = () => {
+  return (
+    <div className="message bot">
+      <div className="typing-indicator">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default ThinkingBubble;
